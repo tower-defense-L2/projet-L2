568a4bd6a70d2741b149de920f70826c297541a5
meo <meo.prn@outlook.fr>
2023-02-09 12:22:32 +0100

    feat(SDL2): compilation fonctionel SDL2
    ca marche maintenant

--- lib/SDL2-2.26.2/build/SDL_render_psp.d
+++
@@ -1,8 +0,0 @@
-build/SDL_render_psp.lo: \
- /home/meo/Documents/GitHub/projet-L2/lib/SDL2-2.26.2/src/render/psp/SDL_render_psp.c \
- /home/meo/Documents/GitHub/projet-L2/lib/SDL2-2.26.2/src/render/psp/../../SDL_internal.h \
- /home/meo/Documents/GitHub/projet-L2/lib/SDL2-2.26.2/src/render/psp/../../dynapi/SDL_dynapi.h \
- /home/meo/Documents/GitHub/projet-L2/lib/SDL2-2.26.2/src/render/psp/../../dynapi/SDL_dynapi_overrides.h \
- include/SDL_config.h include/SDL_platform.h include/begin_code.h \
- include/close_code.h include/SDL_assert.h include/SDL_stdinc.h \
- include/SDL_config.h include/SDL_log.h
